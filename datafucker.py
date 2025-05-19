cb0c6d3971fa7386dd8dc47a72e29440fb345c5e
Bosun <137367418+Bosun@users.noreply.github.com>
2025-05-19 12:56:00 +0800

    Update datafucker.py

--- datafucker.py
+++ datafucker.py
@@ -5,8 +5,8 @@ import numpy as np
 
 def data_generator():
     torch.manual_seed(73)
-    train_data = datasets.MNIST('data', train=True, download=True, transform=transforms.ToTensor())
-    test_data = datasets.MNIST('data', train=False, download=True, transform=transforms.ToTensor())
+    train_data = datasets.MNIST('./data', train=True, download=True, transform=transforms.ToTensor())
+    test_data = datasets.MNIST('./data', train=False, download=True, transform=transforms.ToTensor())
     batch_size = 64
     train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
     test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True)
